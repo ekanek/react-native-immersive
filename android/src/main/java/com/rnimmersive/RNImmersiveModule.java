4ab392e1525ed75a43d9cfc6bef2f69655344998
Neha Gupta <nehagupta0595@gmail.com>
2019-03-20 19:55:13 +0530

    Add Support for -  setting Full immersive mode(In it navigation bar and status bar both disappear) -  setting Partial immersive mode(in it translucent navigation bar appear and status bar hide) -  both are available as immersive sticky

--- android/src/main/java/com/rnimmersive/RNImmersiveModule.java
+++ android/src/main/java/com/rnimmersive/RNImmersiveModule.java
@@ -4,6 +4,8 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.view.View;
+import android.view.Window;
+import android.graphics.Color;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -20,22 +22,27 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
  * {@link NativeModule} that allows changing the appearance of the menu bar.
  */
 public class RNImmersiveModule extends ReactContextBaseJavaModule {
+
   private static final String ERROR_NO_ACTIVITY = "E_NO_ACTIVITY";
   private static final String ERROR_NO_ACTIVITY_MESSAGE = "Tried to set immersive while not attached to an Activity";
-  private static final int UI_FLAG_IMMERSIVE = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-    // remove it as i need to show navigation bar transparent at bottom
-    // | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+  private static final int UI_FLAG_FULL_IMMERSIVE = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+  // In Partial Immersive Mode NavigationBar is visible to user.
+  // But user can made it transparent, translucent and render UI like in FULL
+  // Immersive
+  private static final int UI_FLAG_PARTIAL_IMMERSIVE = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
   private static RNImmersiveModule SINGLETON = null;
 
   private ReactContext _reactContext = null;
   private boolean _isImmersiveOn = false;
 
-  public static RNImmersiveModule getInstance () {
+  public static RNImmersiveModule getInstance() {
     return SINGLETON;
   }
 
@@ -58,25 +65,43 @@ public class RNImmersiveModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void setImmersive(final boolean isOn, final Promise res) {
-    _setImmersive(isOn, res);
+  public void setFullImmersiveMode(final boolean isOn, final Promise res) {
+    _setFullImmersiveMode(isOn, res);
   }
+
   @ReactMethod
-  public void getImmersive(final Promise res) {
-    _getImmersive(res);
+  public void setPartialImmersiveMode(final boolean isOn, final Promise res) {
+    _setPartialImmersiveMode(isOn, res);
   }
+
   @ReactMethod
-  public void addImmersiveListener() {
-    _addImmersiveListener();
+  public void getFullImmersiveState(final Promise res) {
+    _getFullImmersiveState(res);
+  }
+
+  @ReactMethod
+  public void getPartialImmersiveState(final Promise res) {
+    _getPartialImmersiveState(res);
+  }
+
+  @ReactMethod
+  public void addFullImmersiveListener() {
+    _addFullImmersiveListener();
+  }
+
+  @ReactMethod
+  public void addPartialImmersiveListener() {
+    _addPartialImmersiveListener();
   }
 
   public void emitImmersiveStateChangeEvent() {
     if (_reactContext != null && _reactContext.hasActiveCatalystInstance()) {
-      _reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("@@IMMERSIVE_STATE_CHANGED", null);
+      _reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("@@IMMERSIVE_STATE_CHANGED",
+          null);
     }
   }
 
-  private void _setImmersive(final boolean isOn, final Promise res) {
+  private void _setFullImmersiveMode(final boolean isOn, final Promise res) {
     final Activity activity = getCurrentActivity();
     if (activity == null) {
       res.reject(ERROR_NO_ACTIVITY, ERROR_NO_ACTIVITY_MESSAGE);
@@ -89,14 +114,37 @@ public class RNImmersiveModule extends ReactContextBaseJavaModule {
         @Override
         public void run() {
           _isImmersiveOn = isOn;
-          activity.getWindow().getDecorView().setSystemUiVisibility(isOn ? UI_FLAG_IMMERSIVE : View.SYSTEM_UI_FLAG_VISIBLE);
+          activity.getWindow().getDecorView()
+              .setSystemUiVisibility(isOn ? UI_FLAG_FULL_IMMERSIVE : View.SYSTEM_UI_FLAG_VISIBLE);
           res.resolve(null);
         }
       });
     }
   }
 
-  private void _getImmersive(final Promise res) {
+  private void _setPartialImmersiveMode(final boolean isOn, final Promise res) {
+    final Activity activity = getCurrentActivity();
+    if (activity == null) {
+      res.reject(ERROR_NO_ACTIVITY, ERROR_NO_ACTIVITY_MESSAGE);
+      return;
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      UiThreadUtil.runOnUiThread(new Runnable() {
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        public void run() {
+          _isImmersiveOn = isOn;
+          activity.getWindow().setNavigationBarColor(Color.argb(0.3f, 0.0f, 0.0f, 0.0f));
+          activity.getWindow().getDecorView()
+              .setSystemUiVisibility(isOn ? UI_FLAG_PARTIAL_IMMERSIVE : View.SYSTEM_UI_FLAG_VISIBLE);
+          res.resolve(null);
+        }
+      });
+    }
+  }
+
+  private void _getFullImmersiveState(final Promise res) {
     final Activity activity = getCurrentActivity();
     if (activity == null) {
       res.reject(ERROR_NO_ACTIVITY, ERROR_NO_ACTIVITY_MESSAGE);
@@ -109,7 +157,7 @@ public class RNImmersiveModule extends ReactContextBaseJavaModule {
         @Override
         public void run() {
           int visibility = activity.getWindow().getDecorView().getSystemUiVisibility();
-          boolean isImmersiveOn = 0 != (visibility & UI_FLAG_IMMERSIVE);
+          boolean isImmersiveOn = 0 != (visibility & UI_FLAG_FULL_IMMERSIVE);
 
           WritableMap map = Arguments.createMap();
           map.putBoolean("isImmersiveOn", isImmersiveOn);
@@ -120,25 +168,76 @@ public class RNImmersiveModule extends ReactContextBaseJavaModule {
     }
   }
 
-  private void _addImmersiveListener() {
+  private void _getPartialImmersiveState(final Promise res) {
     final Activity activity = getCurrentActivity();
-    if (activity == null) return;
+    if (activity == null) {
+      res.reject(ERROR_NO_ACTIVITY, ERROR_NO_ACTIVITY_MESSAGE);
+      return;
+    }
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
       UiThreadUtil.runOnUiThread(new Runnable() {
         @TargetApi(Build.VERSION_CODES.KITKAT)
         @Override
         public void run() {
-          activity.getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-              boolean isImmersiveOn = 0 != (visibility & UI_FLAG_IMMERSIVE);
+          int visibility = activity.getWindow().getDecorView().getSystemUiVisibility();
+          boolean isImmersiveOn = 0 != (visibility & UI_FLAG_PARTIAL_IMMERSIVE);
+          WritableMap map = Arguments.createMap();
+          map.putBoolean("isImmersiveOn", isImmersiveOn);
 
-              if (isImmersiveOn != _isImmersiveOn) {
-                emitImmersiveStateChangeEvent();
-              }
-            }
-          });
+          res.resolve(map);
+        }
+      });
+    }
+  }
+
+  private void _addFullImmersiveListener() {
+    final Activity activity = getCurrentActivity();
+    if (activity == null)
+      return;
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      UiThreadUtil.runOnUiThread(new Runnable() {
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        public void run() {
+          activity.getWindow().getDecorView()
+              .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                  boolean isImmersiveOn = 0 != (visibility & UI_FLAG_FULL_IMMERSIVE);
+
+                  if (isImmersiveOn != _isImmersiveOn) {
+                    emitImmersiveStateChangeEvent();
+                  }
+                }
+              });
+        }
+      });
+    }
+  }
+
+  private void _addPartialImmersiveListener() {
+    final Activity activity = getCurrentActivity();
+    if (activity == null)
+      return;
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      UiThreadUtil.runOnUiThread(new Runnable() {
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        public void run() {
+          activity.getWindow().getDecorView()
+              .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                  boolean isImmersiveOn = 0 != (visibility & UI_FLAG_PARTIAL_IMMERSIVE);
+
+                  if (isImmersiveOn != _isImmersiveOn) {
+                    emitImmersiveStateChangeEvent();
+                  }
+                }
+              });
         }
       });
     }
--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-import { NativeModules, DeviceEventEmitter, Platform } from 'react-native'
-const { RNImmersive } = NativeModules
+import { NativeModules, DeviceEventEmitter, Platform } from 'react-native';
+
+const { RNImmersive } = NativeModules;
 
 const unSupportedError = __DEV__
-  ? () => { throw new Error('[react-native-immersive] should not be called on iOS') }
-  : () => {}
+  ? () => {
+    throw new Error('[react-native-immersive] should not be called on iOS');
+  }
+  : () => {};
 
-let isListenerEnabled = false
+let isListenerEnabled = false;
 
-const Immersive = Platform.OS === 'android' ? {
-  on: () => RNImmersive.setImmersive(true),
-  off: () => RNImmersive.setImmersive(false),
-  setImmersive: (isOn) => RNImmersive.setImmersive(isOn),
-  getImmersive: () => RNImmersive.getImmersive(), // do not always match actual display state
-  addImmersiveListener: (listener) => {
-    DeviceEventEmitter.addListener('@@IMMERSIVE_STATE_CHANGED', listener)
-    if (isListenerEnabled) return
-    isListenerEnabled = true
-    RNImmersive.addImmersiveListener()
-  },
-  removeImmersiveListener: (listener) => DeviceEventEmitter.removeListener('@@IMMERSIVE_STATE_CHANGED', listener)
-} : {
-  on: unSupportedError,
-  off: unSupportedError,
-  setImmersive: unSupportedError,
-  getImmersive: unSupportedError,
-  addImmersiveListener: unSupportedError,
-  removeImmersiveListener: unSupportedError
-}
+const Immersive = Platform.OS === 'android'
+  ? {
+    on: () => RNImmersive.setFullImmersiveMode(true),
+    off: () => RNImmersive.setFullImmersiveMode(false),
+    setFullImmersiveMode: isOn => RNImmersive.setFullImmersiveMode(isOn),
+    setPartialImmersiveMode: isOn => RNImmersive.setPartialImmersiveMode(isOn),
+    getFullImmersiveState: () => RNImmersive.getFullImmersiveState(), // do not always match actual display state
+    getPartialImmersiveState: () => RNImmersive.getPartialImmersiveState(), // do not always match actual display state
+    addFullImmersiveListener: (listener) => {
+      DeviceEventEmitter.addListener('@@IMMERSIVE_STATE_CHANGED', listener);
+      if (isListenerEnabled) return;
+      isListenerEnabled = true;
+      RNImmersive.addFullImmersiveListener();
+    },
+    addPartialImmersiveListener: (listener) => {
+      DeviceEventEmitter.addListener('@@IMMERSIVE_STATE_CHANGED', listener);
+      if (isListenerEnabled) return;
+      isListenerEnabled = true;
+      RNImmersive.addPartialImmersiveListener();
+    },
+    removeImmersiveListener: listener => DeviceEventEmitter.removeListener('@@IMMERSIVE_STATE_CHANGED', listener),
+  }
+  : {
+    on: unSupportedError,
+    off: unSupportedError,
+    setFullImmersiveMode: unSupportedError,
+    setPartialImmersiveMode: unSupportedError,
+    getFullImmersiveState: unSupportedError,
+    getPartialImmersiveState: unSupportedError,
+    addFullImmersiveListener: unSupportedError,
+    addPartialImmersiveListener: unSupportedError,
+    removeImmersiveListener: unSupportedError,
+  };
 
-export { Immersive }
-export default Immersive
+export { Immersive };
+export default Immersive;
